59ff9444ae492fadd8608196bf7783b9e2f5706d
natalialashko <natasha0909761805@gmail.com>
2023-04-29 23:07:54 +0300

    update3

--- src/components/Main/Main.jsx
+++
@@ -1,7 +0,0 @@
-import { Paragraph } from "../Paragraph"
-
-export const Main = () => {
-    return (<div id="main_id" className="test_class">
-        <Paragraph className="par_class"> hello world </Paragraph>
-    </div>)
-};
